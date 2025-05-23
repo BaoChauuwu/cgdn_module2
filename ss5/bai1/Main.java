c4f85f0f4441c53b9bca8a4fc198faf033b796fb
BaoChauuwu <baochaudeptrai300@gmail.com>
2025-05-23 18:47:10 +0700

    ss6

---
+++ ss5/bai1/Main.java
@@ -0,0 +1,11 @@
+package ss5.bai1;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.setRadius(5);
+        System.out.println( circle.getRadius());
+        System.out.println(circle.getArea());
+
+    }
+}
---
+++ ss5/bai2/Test.java
@@ -0,0 +1,10 @@
+package ss5.bai2;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Nam");
+        student.setClasses("Codegym");
+        System.out.println(student);
+    }
+}
